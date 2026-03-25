05af93b7787fa9124c9b61955bbeceec08c8f1e1
Dan <vfvf5389127@gmail.com>
2026-03-25 14:33:33 +0300

    Added logic

--- backend/Program.cs
+++ backend/Program.cs
@@ -10,4 +10,12 @@ while (true)
     System.Console.WriteLine($"Привет! {name}");
     System.Console.WriteLine($"Сегодня : {DateTime.Now.DayOfWeek}");
 
+    System.Console.WriteLine("Хотите продолжить? (y/n)");
+
+    string answer = Console.ReadLine();
+    if (answer == "n")
+    {
+        System.Console.WriteLine($"Прощайте {name}!");
+        break;
+    }
 }
